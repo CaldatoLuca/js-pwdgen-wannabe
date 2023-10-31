8afbc757cf3a84842c83c92a71d33d1b3e691093
CaldatoLuca <lucacaldato15@gmail.com>
2023-10-31 15:47:53 +0100

    feat: random number bonus

--- pswdgen.js
+++ pswdgen.js
@@ -5,12 +5,13 @@ const elementPassword = document.getElementById("password");
 const elementName = prompt("inserisci il tuo nome");
 const elementSurname = prompt("inserisci il tuo cognome");
 const elementColor = prompt("inserisci il tuo colore preferito");
-const elementNumber = prompt("inserisci il tuo numero preferito");
+const elementNumber = Math.floor(Math.random() * 100);
+// const elementNumber = prompt("inserisci il tuo numero preferito");
 
 console.log("il tuo nome: " + elementName);
 console.log("il tuo cognome: " + elementSurname);
 console.log("il tuo colore preferito: " + elementColor);
-console.log("il tuo numero preferito: " + elementNumber);
+console.log("il tuo password number è: " + elementNumber);
 
 elementPassword.innerHTML =
   elementName + elementSurname + elementColor + elementNumber;
