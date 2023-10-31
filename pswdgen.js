1d856270528ad2638b88d4111efb03b09de993dc
CaldatoLuca <lucacaldato15@gmail.com>
2023-10-31 15:16:21 +0100

    feat: creazioni variabili

--- pswdgen.js
+++ pswdgen.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const elementName = document.getElementById("name");
+const elementSurname = document.getElementById("surname");
+const elementColor = document.getElementById("color");
+const elementNumber = document.getElementById("number");
+
+console.log(elementName);
+console.log(elementSurname);
+console.log(elementColor);
+console.log(elementNumber);
