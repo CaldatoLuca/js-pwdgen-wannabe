15b5e896c20bca9ea116f4049cc9e8b4a7c53a3d
CaldatoLuca <lucacaldato15@gmail.com>
2023-10-31 15:40:46 +0100

    feat: console.log dei miei dati

--- pswdgen.js
+++ pswdgen.js
@@ -7,5 +7,10 @@ const elementSurname = prompt("inserisci il tuo cognome");
 const elementColor = prompt("inserisci il tuo colore preferito");
 const elementNumber = prompt("inserisci il tuo numero preferito");
 
+console.log("il tuo nome: " + elementName);
+console.log("il tuo cognome: " + elementSurname);
+console.log("il tuo colore preferito: " + elementColor);
+console.log("il tuo numero preferito: " + elementNumber);
+
 elementPassword.innerHTML =
   elementName + elementSurname + elementColor + elementNumber;
